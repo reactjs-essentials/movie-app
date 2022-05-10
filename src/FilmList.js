7bce4c2ed4bc94e36c572e1311ce883b6418da1f
João <jmgneves@hotmail.com>
2022-05-10 17:42:14 +0200

    React Routing (#8)
    Co-authored-by: Joao Neves <joao.m.guedes.neves@avanade.com>

--- src/FilmList.js
+++ src/FilmList.js
@@ -2,18 +2,21 @@ import FilmItem from "./FilmItem";
 
 const FilmList = ({ films, handleDeleteFilm }) => {
   return (
-    <div className="film-list">
-      <ul>
-        {films.map((film) => (
-          <li key={film.imdbID}>
-            <FilmItem film={film} />
-            <button onClick={() => handleDeleteFilm(film.imdbID)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h1>Movies list</h1>
+      <div className="film-list">
+        <ul>
+          {films.map((film) => (
+            <li key={film.imdbID}>
+              <FilmItem film={film} />
+              <button onClick={() => handleDeleteFilm(film.imdbID)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
