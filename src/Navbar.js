68d7fec5e139a750d82baa0122b78b940a2e48ce
Miguel Castillo Nolasco <m.nolasco.castillo@avanade.com>
2022-09-29 15:55:48 +0200

    add lab 16 changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,46 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './navbar.css'
+import { useContext } from "react";
+import ThemeContext from "./context/ThemeContext";
+import { withTheme } from "styled-components";
+
 
 const Navbar = () => {
+
+  //const themeContext = useContext(ThemeContext)
+  //console.log(themeContext)
+
+  const { theme, setTheme } = useContext(ThemeContext)
+
+  function handleChangeTheme() {
+    theme && theme == 'light' ? setTheme('dark') : setTheme('light')
+    document.documentElement.style.setProperty("data-theme", theme);
+    console.log(theme)
+  }
+
   return (
-    <section>
-      <nav className="navbar">
-        <div className="links">
-          <ul className="menu">
-            <li>
-              <Link to="/">Films</Link>
-            </li>
-            <li>
-              <Link to="/new">New film</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </section>
+    <>
+      <section>
+        <nav className="navbar">
+          <div className="links">
+            <ul className={theme}>
+              <li>
+                <Link to="/">Films</Link>
+              </li>
+              <li>
+                <Link to="/new">New film</Link>
+              </li>
+              <li>
+                <button onClick={handleChangeTheme}>Change Theme</button>
+              </li>
+              <li>
+                <a>{theme}</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </section>
+    </>
   );
 };
 
