4691443851dc6a9d28499f016a5ba546d87aff52
Miguel Castillo Nolasco <m.nolasco.castillo@avanade.com>
2022-09-30 12:00:55 +0200

    add redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Provider } from 'react-redux'
+import watchlistReducer  from './reducers/watchlistReducer'
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const store = createStore(watchlistReducer)
+
 root.render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
